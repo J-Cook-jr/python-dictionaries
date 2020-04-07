3eccb972ad0b5fec2ca44d517e1b045cb0dc5cf6
Jeff Cook <jecook2692@gmail.com>
2020-04-07 13:07:00 -0400

    adds earlton hotel rooms

--- hotel.py
+++ hotel.py
@@ -1,7 +1,12 @@
+# This program creates a dictionary of hotel rooms and it's occupants.
+
+# Create a dictionary that lists the room number and it's occupants.
 earlton_hotel ={
- "101" : "Harley Cook",
-"102" : "Mildred Tatum",
-"103" : "Jewel Cook",
-"104" : "Tiffany Waters",
-"105" : "Dejon Waters" 
+    "Room 101" : "Harley Cook",
+    "Room 102" : "Mildred Tatum",
+    "Room 103" : "Jewel Cook",
+    "Room 104" : "Tiffany Waters",
+    "Room 105" : "Dejon Waters" 
 }
+
+print(earlton_hotel)
